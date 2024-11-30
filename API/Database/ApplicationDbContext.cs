593fdab77656e934e69f3c20e6630585e425a959
brayd <braydenms@outlook.com>
2024-11-30 09:46:39 -0500

    make sure ApplicationDbContext.cs is there

---
+++ API/Database/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Schedule.Database.Entities;
+
+namespace Schedule.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Shift> Shifts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure entity properties and relationships here
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Password).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+                entity.Property(e => e.LastName).HasMaxLength(50);
+                entity.Property(e => e.Email).HasMaxLength(100);
+                entity.Property(e => e.Role).IsRequired();
+            });
+
+            modelBuilder.Entity<Shift>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.DateTime).IsRequired();
+                entity.Property(e => e.Role).IsRequired();
+                entity.Property(e => e.Username).HasMaxLength(50);
+            });
+        }
+    }
+}
